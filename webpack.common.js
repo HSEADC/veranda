8f68638488007cc4ead1105288e03a6a23b8f379
maxim kio <enderbroxa@yandex.ru>
2023-10-24 11:44:50 +0300

    first commit

--- webpack.common.js
+++ webpack.common.js
@@ -1,109 +1,140 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPartialsPlugin = require("html-webpack-partials-plugin");
 
-const webpack = require('webpack')
-const path = require('path')
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
   entry: {
-    index: './src/index.js',
-    page: './src/page.jsx'
+    index: "./src/index.js",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'docs')
-    // clean: true
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "docs"),
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/i,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties']
-          }
-        }
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
       },
       {
-        test: /\.(sa|sc|c)ss$/i,
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
+          "css-loader",
+          "sass-loader",
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [['postcss-preset-env']]
-              }
-            }
+                plugins: [["postcss-preset-env"]],
+              },
+            },
           },
-          'sass-loader'
-        ]
+        ],
       },
       {
         test: /\.html$/i,
-        loader: 'html-loader'
+        loader: "html-loader",
       },
       {
         resourceQuery: /raw/,
-        type: 'asset/source'
+        type: "asset/source",
       },
       {
-        test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: 'asset/resource',
+        test: /\.(png|svg|jpg|jpeg|webp)$/i,
+        type: "asset/resource",
         generator: {
-          filename: 'images/[hash][ext][query]'
-        }
+          filename: "images/[hash][ext][query]",
+        },
       },
       {
         test: /\.(ttf|otf|woff|woff2)$/i,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: 'fonts/[name].[ext]'
-        }
-      }
-    ]
+          name: "fonts/[name].[ext]",
+        },
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
     }),
 
-    // Landing page
+    // Index
     new HtmlWebpackPlugin({
-      hash: true,
-      scriptLoading: 'blocking',
-      template: './src/index.html',
-      filename: './index.html',
-      chunks: ['index']
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/about.html",
+      filename: "./about.html",
     }),
 
-    // Internal pages
+    // Section
     new HtmlWebpackPlugin({
-      hash: true,
-      scriptLoading: 'blocking',
-      template: './src/pages/page.html',
-      filename: './pages/page.html',
-      chunks: ['page']
+      template: "./src/articles.html",
+      filename: "./articles.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/gallery.html",
+      filename: "./gallery.html",
+    }),
+    // Article
+    new HtmlWebpackPlugin({
+      template: "./src/articles/glamping.html",
+      filename: "./articles/glamping.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/articles/advice.html",
+      filename: "./articles/advice.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/gallery/house.html",
+      filename: "./gallery/house.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/gallery/garden.html",
+      filename: "./gallery/garden.html",
     }),
 
     // Partials
     new HtmlWebpackPartialsPlugin([
       {
-        path: path.join(__dirname, './src/partials/analytics.html'),
-        location: 'analytics',
-        template_filename: '*',
-        priority: 'replace'
-      }
-    ])
+        path: path.join(__dirname, "./src/partials/analytics.html"),
+        location: "analytics",
+        template_filename: "*",
+        priority: "replace",
+      },
+    ]),
   ],
   optimization: {
-    minimizer: [new CssMinimizerPlugin()]
-  }
-}
+    minimizer: [new CssMinimizerPlugin()],
+  },
+};
